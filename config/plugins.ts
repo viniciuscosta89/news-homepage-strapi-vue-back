2e8f844733fe4389904ae999770213f7c40b704c
Vinicius Costa <vinicius.costa89@gmail.com>
2023-07-20 21:13:56 -0300

    feat: :sparkles: production setup with Docker and postgres; uploads on Cloudinary

---
+++ config/plugins.ts
@@ -0,0 +1,21 @@
+export default ({ env }) => ({
+	'import-export-entries': {
+		enabled: true,
+		config: {},
+	},
+	upload: {
+		config: {
+			provider: 'cloudinary',
+			providerOptions: {
+				cloud_name: env('CLOUDINARY_NAME'),
+				api_key: env('CLOUDINARY_KEY'),
+				api_secret: env('CLOUDINARY_SECRET'),
+			},
+			actionOptions: {
+				upload: {},
+				uploadStream: {},
+				delete: {},
+			},
+		},
+	},
+});
